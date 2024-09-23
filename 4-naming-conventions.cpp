fb403ffdd358ff35d65981f7ac590e04a0d2a12f
Gökhan Koç <gokhan_koc@windowslive.com>
2024-09-23 19:51:08 +0000

    Math

---
+++ 4-naming-conventions.cpp
@@ -0,0 +1,6 @@
+int main() {
+    int file_size; // Snake Case
+    int FileSize; // Pascal Case
+    int fileSize; // Camel Case
+    int iFileSize; // Hungarian Notation - not used anymore
+}
---
+++ 5-math-expressions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    double x = 10;
+    int y = 3;
+    int z = x + y;
+
+    std::cout << z;
+    return 0;
+}
